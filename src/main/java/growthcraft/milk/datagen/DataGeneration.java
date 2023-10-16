44dc34a4c925073c4f70b5395588d1e432486ec8
Alatyami <alatyami@gmail.com>
2023-10-15 19:52:06 -0600

    Fixed auto-subscribe event for data generation.

--- src/main/java/growthcraft/milk/datagen/DataGeneration.java
+++ src/main/java/growthcraft/milk/datagen/DataGeneration.java
@@ -2,18 +2,15 @@ package growthcraft.milk.datagen;
 
 import growthcraft.milk.datagen.loot.GrowthcraftMilkLootTables;
 import growthcraft.milk.init.GrowthcraftMilkItems;
-import growthcraft.milk.shared.Reference;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.minecraftforge.client.event.ModelEvent;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = Reference.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+//@Mod.EventBusSubscriber(modid = Reference.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DataGeneration {
-    @SubscribeEvent
+    //@SubscribeEvent
     public static void generate(GatherDataEvent event) {
         DataGenerator generator = event.getGenerator();
         PackOutput output = generator.getPackOutput();
@@ -29,7 +26,7 @@ public class DataGeneration {
 
     }
 
-    @SubscribeEvent
+    //@SubscribeEvent
     public static void modelBake(ModelEvent.ModifyBakingResult event) {
         GrowthcraftMilkItems.addItemModelProperties();
     }
