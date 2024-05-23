76ddc66bd0b7f3a6cb0df200e42c51d31bcaad42
Alatyami <alatyami@gmail.com>
2024-05-23 08:41:06 -0600

    #97 Fixed Bee Box DataGen Recipes, added cherry and mangrove bee boxes
    Added recipe building, advancements, and JSON files for cherry and mangrove bee boxes. Prior incorrect usage of acacia items for oak bee box has been corrected to use oak items. Refactored bee box recipe building into a separate method for better code clarity and re-usability.

--- src/main/java/growthcraft/apiary/datagen/providers/GrowthcraftApiaryRecipes.java
+++ src/main/java/growthcraft/apiary/datagen/providers/GrowthcraftApiaryRecipes.java
@@ -15,8 +15,11 @@ import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.data.recipes.ShapelessRecipeBuilder;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.Tags;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -28,97 +31,31 @@ public class GrowthcraftApiaryRecipes extends RecipeProvider{
 
 	@Override
 	protected void buildRecipes(Consumer<FinishedRecipe> consumer) {
-		ShapedRecipeBuilder.shaped(RecipeCategory.DECORATIONS, GrowthcraftApiaryBlocks.BEE_BOX_ACACIA.get())
-		.pattern("SPS")
-		.pattern("PBP")
-		.pattern("SPS")
-		.define('B', Items.ACACIA_SLAB)
-		.define('P', Items.ACACIA_PLANKS)
-		.define('S', Tags.Items.RODS_WOODEN)
-		.group(Reference.MODID)
-		.unlockedBy("has_item", InventoryChangeTrigger.TriggerInstance.hasItems(Items.ACACIA_PLANKS))
-		.save(consumer);
-		
-		ShapedRecipeBuilder.shaped(RecipeCategory.DECORATIONS, GrowthcraftApiaryBlocks.BEE_BOX_BIRCH.get())
-		.pattern("SPS")
-		.pattern("PBP")
-		.pattern("SPS")
-		.define('B', Items.BIRCH_SLAB)
-		.define('P', Items.BIRCH_PLANKS)
-		.define('S', Tags.Items.RODS_WOODEN)
-		.group(Reference.MODID)
-		.unlockedBy("has_item", InventoryChangeTrigger.TriggerInstance.hasItems(Items.BIRCH_PLANKS))
-		.save(consumer);
-		
-		ShapedRecipeBuilder.shaped(RecipeCategory.DECORATIONS, GrowthcraftApiaryBlocks.BEE_BOX_CRIMSON.get())
-		.pattern("SPS")
-		.pattern("PBP")
-		.pattern("SPS")
-		.define('B', Items.CRIMSON_SLAB)
-		.define('P', Items.CRIMSON_PLANKS)
-		.define('S', Tags.Items.RODS_WOODEN)
-		.group(Reference.MODID)
-		.unlockedBy("has_item", InventoryChangeTrigger.TriggerInstance.hasItems(Items.CRIMSON_PLANKS))
-		.save(consumer);
-		
-		ShapedRecipeBuilder.shaped(RecipeCategory.DECORATIONS, GrowthcraftApiaryBlocks.BEE_BOX_DARK_OAK.get())
-		.pattern("SPS")
-		.pattern("PBP")
-		.pattern("SPS")
-		.define('B', Items.DARK_OAK_SLAB)
-		.define('P', Items.DARK_OAK_PLANKS)
-		.define('S', Tags.Items.RODS_WOODEN)
-		.group(Reference.MODID)
-		.unlockedBy("has_item", InventoryChangeTrigger.TriggerInstance.hasItems(Items.DARK_OAK_PLANKS))
-		.save(consumer);
-		
-		ShapedRecipeBuilder.shaped(RecipeCategory.DECORATIONS, GrowthcraftApiaryBlocks.BEE_BOX_JUNGLE.get())
-		.pattern("SPS")
-		.pattern("PBP")
-		.pattern("SPS")
-		.define('B', Items.JUNGLE_SLAB)
-		.define('P', Items.JUNGLE_PLANKS)
-		.define('S', Tags.Items.RODS_WOODEN)
-		.group(Reference.MODID)
-		.unlockedBy("has_item", InventoryChangeTrigger.TriggerInstance.hasItems(Items.JUNGLE_PLANKS))
-		.save(consumer);
-		
-		ShapedRecipeBuilder.shaped(RecipeCategory.DECORATIONS, GrowthcraftApiaryBlocks.BEE_BOX_OAK.get())
-		.pattern("SPS")
-		.pattern("PBP")
-		.pattern("SPS")
-		.define('B', Items.ACACIA_SLAB)
-		.define('P', Items.ACACIA_PLANKS)
-		.define('S', Tags.Items.RODS_WOODEN)
-		.group(Reference.MODID)
-		.unlockedBy("has_item", InventoryChangeTrigger.TriggerInstance.hasItems(Items.ACACIA_PLANKS))
-		.save(consumer);
-		
-		ShapedRecipeBuilder.shaped(RecipeCategory.DECORATIONS, GrowthcraftApiaryBlocks.BEE_BOX_SPRUCE.get())
-		.pattern("SPS")
-		.pattern("PBP")
-		.pattern("SPS")
-		.define('B', Items.SPRUCE_SLAB)
-		.define('P', Items.SPRUCE_PLANKS)
-		.define('S', Tags.Items.RODS_WOODEN)
-		.group(Reference.MODID)
-		.unlockedBy("has_item", InventoryChangeTrigger.TriggerInstance.hasItems(Items.SPRUCE_PLANKS))
-		.save(consumer);
-		
-		ShapedRecipeBuilder.shaped(RecipeCategory.DECORATIONS, GrowthcraftApiaryBlocks.BEE_BOX_WARPED.get())
-		.pattern("SPS")
-		.pattern("PBP")
-		.pattern("SPS")
-		.define('B', Items.WARPED_SLAB)
-		.define('P', Items.WARPED_PLANKS)
-		.define('S', Tags.Items.RODS_WOODEN)
-		.group(Reference.MODID)
-		.unlockedBy("has_item", InventoryChangeTrigger.TriggerInstance.hasItems(Items.WARPED_PLANKS))
-		.save(consumer);
-		
+		// Bee Box Recipes
+		this.beeBoxRecipeBuilder(consumer, GrowthcraftApiaryBlocks.BEE_BOX_ACACIA.get(),
+				Items.ACACIA_SLAB, Items.ACACIA_PLANKS, Tags.Items.RODS_WOODEN);
+		this.beeBoxRecipeBuilder(consumer, GrowthcraftApiaryBlocks.BEE_BOX_BIRCH.get(),
+				Items.BIRCH_SLAB, Items.BIRCH_PLANKS, Tags.Items.RODS_WOODEN);
+		this.beeBoxRecipeBuilder(consumer, GrowthcraftApiaryBlocks.BEE_BOX_CHERRY.get(),
+				Items.CHERRY_SLAB, Items.CHERRY_PLANKS, Tags.Items.RODS_WOODEN);
+		this.beeBoxRecipeBuilder(consumer, GrowthcraftApiaryBlocks.BEE_BOX_CRIMSON.get(),
+				Items.CRIMSON_SLAB, Items.CRIMSON_PLANKS, Tags.Items.RODS_WOODEN);
+		this.beeBoxRecipeBuilder(consumer, GrowthcraftApiaryBlocks.BEE_BOX_DARK_OAK.get(),
+				Items.DARK_OAK_SLAB, Items.DARK_OAK_PLANKS, Tags.Items.RODS_WOODEN);
+		this.beeBoxRecipeBuilder(consumer, GrowthcraftApiaryBlocks.BEE_BOX_JUNGLE.get(),
+				Items.JUNGLE_SLAB, Items.JUNGLE_PLANKS, Tags.Items.RODS_WOODEN);
+		this.beeBoxRecipeBuilder(consumer, GrowthcraftApiaryBlocks.BEE_BOX_MANGROVE.get(),
+				Items.MANGROVE_SLAB, Items.MANGROVE_PLANKS, Tags.Items.RODS_WOODEN);
+		this.beeBoxRecipeBuilder(consumer, GrowthcraftApiaryBlocks.BEE_BOX_OAK.get(),
+				Items.OAK_SLAB, Items.OAK_PLANKS, Tags.Items.RODS_WOODEN);
+		this.beeBoxRecipeBuilder(consumer, GrowthcraftApiaryBlocks.BEE_BOX_SPRUCE.get(),
+				Items.SPRUCE_SLAB, Items.SPRUCE_PLANKS, Tags.Items.RODS_WOODEN);
+		this.beeBoxRecipeBuilder(consumer, GrowthcraftApiaryBlocks.BEE_BOX_WARPED.get(),
+				Items.WARPED_SLAB, Items.WARPED_PLANKS, Tags.Items.RODS_WOODEN);
+
+		// Bees Wax
 		GrowthcraftApiaryItems.ITEMS.getEntries().forEach(item -> {
 			if(item.getId().getPath().contains("bees_wax_")) {
-				
 				ShapedRecipeBuilder.shaped(RecipeCategory.MISC, item.get(), 8)
 				.pattern("AAA")
 				.pattern("ABA")
@@ -130,7 +67,8 @@ public class GrowthcraftApiaryRecipes extends RecipeProvider{
 				.save(consumer);
 			}
 		});
-		
+
+		// Candle
 		ShapedRecipeBuilder.shaped(RecipeCategory.DECORATIONS, Items.CANDLE)
 		.pattern("S")
 		.pattern("H")
@@ -138,9 +76,9 @@ public class GrowthcraftApiaryRecipes extends RecipeProvider{
 		.define('H', GrowthcraftApiaryTags.Items.HONEY_COMB)
 		.unlockedBy("has_item", InventoryChangeTrigger.TriggerInstance.hasItems(Items.HONEYCOMB))
 		.save(consumer);
-		
+
+		// Dyed Candles
 		for (DyeColor dye : DyeColor.values()) {
-			
 			ShapelessRecipeBuilder.shapeless(RecipeCategory.DECORATIONS, ForgeRegistries.ITEMS.getValue(new ResourceLocation("minecraft:"+dye+"_candle")))
 			.requires(Items.CANDLE)
 			.requires(dye.getTag())
@@ -149,7 +87,29 @@ public class GrowthcraftApiaryRecipes extends RecipeProvider{
 			.save(consumer);
 		}
 	}
-	
+
+	/**
+	 * Builds a recipe for creating a bee box using the provided parameters.
+	 *
+	 * @param consumer  The consumer to accept the finished recipe.
+	 * @param beeBoxBlock  The block representing the bee box.
+	 * @param slab  The item representing the slab.
+	 * @param planks  The item representing the planks.
+	 * @param tagRod  The tag key for the rod item.
+	 */
+	private void beeBoxRecipeBuilder(Consumer<FinishedRecipe> consumer, Block beeBoxBlock, Item slab, Item planks, TagKey<Item> tagRod) {
+		ShapedRecipeBuilder.shaped(RecipeCategory.DECORATIONS, beeBoxBlock)
+				.pattern("SPS")
+				.pattern("PBP")
+				.pattern("SPS")
+				.define('B', slab)
+				.define('P', planks)
+				.define('S', tagRod)
+				.group(Reference.MODID)
+				.unlockedBy("has_item", InventoryChangeTrigger.TriggerInstance.hasItems(planks))
+				.save(consumer);
+	}
+
     @Override
     public String getName() {
         return "Growthcraft Apiary Recipes";
